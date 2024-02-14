14dbdd94e3ee512e832be586efe2615a1f7cab80
GiftChrisJava <chrisjava77@gmail.com>
2024-02-14 17:26:01 +0200

    added side navigation bar

---
+++ app/(router)/_components/SideNav.jsx
@@ -0,0 +1,35 @@
+import { BadgeIcon, BookOpen, GraduationCap } from "lucide-react";
+import React from "react";
+
+function SideNav() {
+  const menu = [
+    { id: 1, name: "All Courses", icon: BookOpen },
+
+    { id: 2, name: "Membership", icon: BadgeIcon },
+
+    { id: 1, name: "Be Instructor", icon: GraduationCap },
+  ];
+  return (
+    <div className="p-5 bg-white shadow-sm border h-screen">
+      <p className="font-bold text-md">Education</p>
+      <hr className="mt-7" />
+      {/* menu list  */}
+      <div className="mt-5">
+        {menu.map((item) => (
+          <div
+            key={item.id}
+            className="group flex gap-3 mt-2 text-[18px] items-center 
+          text-gray-500 cursor-pointer hover:bg-primary hover:text-white 
+          rounded-md transition-all ease-in-out duration-200"
+          >
+            <item.icon className="group-hover:animate-bounce" />
+            <h2>{item.name}</h2>
+          </div>
+        ))}
+      </div>
+      S
+    </div>
+  );
+}
+
+export default SideNav;
